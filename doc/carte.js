a8d96bd800cbfab4a7e51e2c4489b4b76a517456
ThoMaCarto <49810626+ThoMaCarto@users.noreply.github.com>
2020-07-12 19:35:41 +0200

    maj db

--- doc/carte.js
+++ doc/carte.js
@@ -111,9 +111,9 @@ $.get('doc/db_img_blayais.csv', function(csvContents) {
     var geoLayer = L.geoCsv(csvContents, {
 		firstLineTitles: true, 
 		fieldSeparator: ';',
-		titles: ['name','Filename','Date','Auteur','User comment','lat', 'lng'],
+		titles: ['name','Filename','Date','Auteur','User comment','cat','lat', 'lng'],
 		onEachFeature:function (feature,layer){
-			layer.bindPopup('<b>'+feature.properties.name+'</b><br/><b>Coordonnées:</b>'+feature.geometry.coordinates+'<br/><img src="'+feature.properties.filename+'" alt="test" width="300"><br/><small>photographie © '+feature.properties.auteur+'</small><br/><p>'+feature.properties.user_comment+'</p>');
+			layer.bindPopup('<b>'+feature.properties.name+'</b><br/><b>Coordonnées:</b> '+feature.geometry.coordinates+'<br/><img src="'+feature.properties.filename+'" alt="test" width="300"><br/><small>photographie © '+feature.properties.auteur+'</small><br/><p>'+feature.properties.user_comment+'</p>');
 			
 		}
 		
